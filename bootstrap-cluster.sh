e5dcadce80e4ca1ee2faf3025b2793773f0c050d
Emma Drake-Snoeijs <emma@deity.io>
2021-07-24 20:51:44 +0200

    switched from jaeger to tempo

--- bootstrap-cluster.sh
+++ bootstrap-cluster.sh
@@ -2,9 +2,6 @@
 
 kubectl apply -f monitoring/namespace.yml
 
-# we don't use the helm chart because we don't need a cluster, just one node.
-kubectl apply -f monitoring/elastic/app.yaml -n monitoring
-
 # prom and grafana
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus prometheus-community/kube-prometheus-stack \
@@ -13,17 +10,15 @@ helm install prometheus prometheus-community/kube-prometheus-stack \
     --wait \
     --timeout 10m
 
-# jaeger
-helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
-helm install jaeger jaegertracing/jaeger \
-  --values monitoring/jaeger/values.yaml \
-	--namespace monitoring
-
 # loki and promtail
 helm repo add grafana https://grafana.github.io/helm-charts
 helm install loki grafana/loki-stack  \
   --values monitoring/loki-stack/values.yaml \
   --namespace monitoring
 
+
+helm repo add tempo https://grafana.github.io/helm-charts 
+helm install tempo grafana/tempo  \
+  --namespace monitoring
+
 echo "http://grafana.localhost"
-echo "http://jaeger.localhost"
